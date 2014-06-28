58fd72f331740e7b27dc7f571f7a3528581e74f1
BennyRa <bennyra92@gmail.com>
2014-06-28 17:37:29 +0200

    Multiple choice selections are shuffled now before sending to the client.

--- Server/ExerciseServer.py
+++ Server/ExerciseServer.py
@@ -6,21 +6,24 @@ import urlparse
 import ExerciseHandler
 import UserHandler
 import CustomHandler
+import random
 
 try:
-	HOST_NAME = sys.argv[1]
-	PORT = int(sys.argv[2])
+    HOST_NAME = sys.argv[1]
+    PORT = int(sys.argv[2])
 except IndexError:
-	print "Using default values. Host = 127.0.0.1, Port = 4242"
-	HOST_NAME = '127.0.0.1'
-	PORT = 4242
+    print "Using default values. Host = 127.0.0.1, Port = 4242"
+    HOST_NAME = '192.168.178.36'
+    PORT = 4242
+
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     escapeSQL = False
 
     def do_GET(self):
         print "in do_get"
-        self.setHeaders([{'type': 'Access-Control-Allow-Origin', 'value': 'http://localhost'}, {'type': 'CONTENT-TYPE', 'value': 'application/json'}])
+        self.setHeaders([{'type': 'Access-Control-Allow-Origin', 'value': '*'},
+                         {'type': 'CONTENT-TYPE', 'value': 'application/json'}])
         path = self.path.split("?")[0]
         print path
         if path == '/questions/':
@@ -30,16 +33,17 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         elif path == '/results/':
             self.sendResults()
         else:
-        	CustomHandler.handleGetRequest(self);
+            CustomHandler.handleGetRequest(self);
 
     def do_POST(self):
         print "path " + self.path
-        self.setHeaders([{'type': 'Access-Control-Allow-Origin', 'value': 'http://localhost'}, {'type': 'CONTENT-TYPE', 'value': 'application/json'}])
+        self.setHeaders([{'type': 'Access-Control-Allow-Origin', 'value': '*'},
+                         {'type': 'CONTENT-TYPE', 'value': 'application/json'}])
         path = self.path.split("?")[0]
         if path == '/questionAnswer/':
             self.giveAnswer()
         else:
-        	CustomHandler.handlePostRequest(self);
+            CustomHandler.handlePostRequest(self);
 
     def setHeaders(self, headers):
         self.send_response(200)
@@ -57,35 +61,51 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write(json.dumps({'numberExercises': len(ExerciseHandler.exercises)}))
 
     def sendQuestion(self):
-        parameters = urlparse.parse_qs(urlparse.urlparse(self.path).query)
-        print parameters
-        exercise = ExerciseHandler.exercises[int(parameters['questionNumber'][0])]
+        try:
+            parameters = urlparse.parse_qs(urlparse.urlparse(self.path).query)
+            print parameters
 
-        user = UserHandler.getUserByName(parameters['user'][0])
-        userAnswer = ''
+            additionalMessage = 'okay'
+            questionNumber = int(parameters['questionNumber'][0])
+            if len(ExerciseHandler.exercises) - 1 < questionNumber:
+                questionNumber = len(ExerciseHandler.exercises) - 1
+                additionalMessage = 'Last question reached!'
 
-        if user is not None:
-            questionAnswer = user.checkIfAlreadyAnswered(exercise.id)
-            if questionAnswer is not None:
-                userAnswer = questionAnswer['answer']
+            exercise = ExerciseHandler.exercises[questionNumber]
 
-        if exercise.type == 'freeText':
-            self.wfile.write(json.dumps({'id': exercise.id, 'type': 'freeText', 'question': exercise.exerciseText, 'givenAnswer': userAnswer}))
-        elif exercise.type == 'multipleChoice':
-            answerSelection = []
-            for correctAnswer in exercise.correctAnswers:
-                answerSelection.append(correctAnswer)
-            for wrongAnswer in exercise.wrongAnswers:
-                answerSelection.append(wrongAnswer)
-            self.wfile.write(json.dumps({'id': exercise.id, 'type': 'multipleChoice', 'question': exercise.exerciseText,
-                                         'answerSelection': answerSelection, 'givenAnswer': userAnswer}))
+            user = UserHandler.getUserByName(parameters['user'][0])
+            userAnswer = ''
+
+            if user is not None:
+                questionAnswer = user.checkIfAlreadyAnswered(exercise.id)
+                if questionAnswer is not None:
+                    userAnswer = questionAnswer['answer']
+
+            if exercise.type == 'freeText':
+                self.wfile.write(json.dumps({'id': exercise.id, 'type': 'freeText', 'question': exercise.exerciseText,
+                                             'givenAnswer': userAnswer, 'additionalMessage': additionalMessage}))
+            elif exercise.type == 'multipleChoice':
+                answerSelection = []
+                for correctAnswer in exercise.correctAnswers:
+                    answerSelection.append(correctAnswer)
+                for wrongAnswer in exercise.wrongAnswers:
+                    answerSelection.append(wrongAnswer)
+                random.shuffle(answerSelection)
+                self.wfile.write(
+                    json.dumps({'id': exercise.id, 'type': 'multipleChoice', 'question': exercise.exerciseText,
+                                'answerSelection': answerSelection, 'givenAnswer': userAnswer,
+                                'additionalMessage': additionalMessage}))
+
+        except IndexError as e:
+            print str(e)
 
     def giveAnswer(self):
         self.setHeaders([{'type': 'CONTENT-TYPE', 'value': 'application/json'}])
         jsonBody = self.readPayload()
         isCorrect = ExerciseHandler.checkAnswer(jsonBody['questionId'], jsonBody['answer'])
         parameters = urlparse.parse_qs(urlparse.urlparse(self.path).query)
-        UserHandler.setQuestionAnswer(parameters['user'][0], parameters['questionNumber'][0], jsonBody['answer'], isCorrect)
+        UserHandler.setQuestionAnswer(parameters['user'][0], parameters['questionNumber'][0], jsonBody['answer'],
+                                      isCorrect)
 
     def sendResults(self):
         questions = []
@@ -101,13 +121,16 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                         answeredCounter += 1
                         if answeredExercise['isCorrect'] == True:
                             answeredCorrectlyCounter += 1
-                        answers.append({'user': user.userName, 'answer': answeredExercise['answer'], 'isCorrect': answeredExercise['isCorrect']})
+                        answers.append({'user': user.userName, 'answer': answeredExercise['answer'],
+                                        'isCorrect': answeredExercise['isCorrect']})
 
             question = {'exercise': exercise.exerciseText, 'correctAnswers': exercise.correctAnswers,
-                        'answeredCorrectlyCounter': answeredCorrectlyCounter, 'answeredCounter': answeredCounter, 'answers': answers}
+                        'answeredCorrectlyCounter': answeredCorrectlyCounter, 'answeredCounter': answeredCounter,
+                        'answers': answers}
             questions.append(question)
 
         self.wfile.write(json.dumps(questions))
+
 
 if __name__ == '__main__':
     exercisesPath = "./exercises.csv"
